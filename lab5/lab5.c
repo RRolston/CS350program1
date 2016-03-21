ea2e997ce1e1ca160d63044d81874601133949b4
RRolston <rqr5099@gmail.com>
2016-03-20 21:08:55 -0400

    working beta lab4 and lab5 //needs a readme

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -9,6 +9,7 @@
 #include<string.h>
 #include<time.h>
 
+#define MAX_PID 32768//max on my personal system
 
 int main(int argc, char *argv[]) {
 
@@ -27,20 +28,55 @@ FILE * inputFile;
 inputFile = fopen(argv[2],"r");
 char *ptr;
 const unsigned int frames_of_memory = strtol(argv[1], &ptr, 10);
-unsigned long int FIFO_page_faults=0;
-unsigned int process_number;
+unsigned long int page_faults=0;//no page replacement
+long int process_number;
 unsigned int address_space_size;
+long int RAM = (long int*) malloc(sizeof(unsigned int)*frames_of_memory);
+unsigned int j;
+unsigned int i;
+for (j = 0; j < frames_of_memory; j++){
+  Pid[j] = -1;
+}
+unsigned int space_left=frames_of_memory;
+unsigned int counter=0;
 unsigned int virtual_page_number;
 int z;//
 char command[10];
 while(1){
   z= fscanf(inputFile, "%s ",command);
   if( z == EOF ) break;
-  if(strcmp(command,"START")==0) fscanf(inputFile, "%u %u\n", &process_number, &address_space_size);
-  if(strcmp(command,"TERMINATE")==0) fscanf(inputFile, "%u\n", &process_number);
-  if(strcmp(command,"REFERENCE")==0) fscanf(inputFile, "%u %u\n", &process_number,&virtual_page_number);
-  printf("%s %u\n", command, process_number );
+  if(strcmp(command,"START")==0){
+     fscanf(inputFile, "%ld %u\n", &process_number, &address_space_size);
+     for (j = counter; j < address_space_size; j++){
+       if(RAM[j]==-1){
+         RAM[j] = process_number;
+         space_left--;
+       }else{
+         address_space_size++;
+       }
+       if(space_left==0){
+         for(i=0; i<frames_of_memory;i++){
+           if(RAM[i]==process_number) RAM[i]=-1;
+         }
+         page_faults++;
+         break;
+       }
+       if((j+1)==frames_of_memory){
+         address_space_size=address_space_size-j;
+         j=-1;
+       }
+     }
+  }
+  if(strcmp(command,"TERMINATE")==0){
+    fscanf(inputFile, "%ld\n", &process_number);
+    for(i=0; i<frames_of_memory;i++){
+      if(RAM[i]==process_number) RAM[i]=-1;
+    }
+  }
+  if(strcmp(command,"REFERENCE")==0) fscanf(inputFile, "%ld %u\n", &process_number,&virtual_page_number);
+  printf("%s %ld\n", command, process_number );
 }
+printf("PAGE FAULTS: %ld\n", page_faults );
 fclose(inputFile);
 //-----------------------------------------------------------
 
